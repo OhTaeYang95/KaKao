dbd0ffe2b51e01d21c76880c2b00eaee6171213d
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-16 23:33:23 +0900

    sdfdsf

--- sw.js
+++ sw.js
@@ -1,16 +1,12 @@
 "use strict";
-
 window.onload = function (e) {
-
-}
-
+};
 Notification.requestPermission(result => {
     console.log("알림성공");
     if (result === 'granted') {
-        showNotification('So nice to have you here!', 'Hey there!')
+        showNotification('So nice to have you here!', 'Hey there!');
     }
 });
-
 function showNotification(title, message) {
     if ('Notification' in window) {
         navigator.serviceWorker.ready.then(registration => {
@@ -21,55 +17,45 @@ function showNotification(title, message) {
         });
     }
 }
-
 export const PUSH_APPLICATION_SERVER_KEY = 'AAAANVHcwbk:APA91bEDFGpqgjIBBrbq0MRwmtJsOPmhVo0hP3xc6OXrvjl7sPwFWEhyhhDFs0JcjE1e7hne1CCW9GMmuV9GggbqYq982yNQoUE7Ofh2Y5EZBdmRc9k37FsEJaTAxA1rfNoErWck4Adr';
-
 // 해시 처리
-const urlB64ToUint8Array = (base64String: string) => {
+const urlB64ToUint8Array = (base64String) => {
     const padding = '='.repeat((4 - base64String.length % 4) % 4);
     const base64 = (base64String + padding)
         .replace(/\-/g, '+')
         .replace(/_/g, '/');
-
     const rawData = window.atob(base64);
     const outputArray = new Uint8Array(rawData.length);
-
     for (let i = 0; i < rawData.length; ++i) {
         outputArray[i] = rawData.charCodeAt(i);
     }
     return outputArray;
 };
-
 // 구독하기
 export const subscribeUser = (swRegistration) => {
-
     const applicationServerKey = urlB64ToUint8Array(PUSH_APPLICATION_SERVER_KEY);
     const ACCESS_PUSH_TOKEN = 'ACCESS_PUSH_TOKEN';
-
     swRegistration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: applicationServerKey, // 서버 키 등록
+        applicationServerKey: applicationServerKey,
     }).then((subscription) => {
         const pwaSubscription = JSON.parse(JSON.stringify(subscription));
         localStorage.setItem(ACCESS_PUSH_TOKEN, pwaSubscription.keys.auth); // 추후 코드 제거를 위해 저장합니다.
         pushSubscription(pwaSubscription);
     }).catch(e => console.log(`subscribe error`, e));
 };
-
 export const pushSubscription = (subscription) => {
     // 서버로 구독 정보 전송 
 };
-
-
-
 if ('serviceWorker' in navigator && 'PushManager' in window) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('https://ohtaeyang95.github.io/KaKao/service-worker.js')
             .then(function (success) {
-                console.log('[Service Worker 등록 완료]', success)
+                console.log('[Service Worker 등록 완료]', success);
             })
             .catch(function (error) {
                 console.log('[Service Worker 등록 실패]', error);
             });
     });
 }
+
