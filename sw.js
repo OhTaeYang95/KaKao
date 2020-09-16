4c884de1c3af1c7ff3374ca76003e488a69340cc
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-16 23:05:03 +0900

    dsfsdf

---
+++ sw.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log("알림성공");
+window.onload = function (e) {
+    console.log("알림성공");
+    Notification.requestPermission().then(function (result) {
+        if (result === 'granted') {
+            randomNotification();
+        }
+    });
+}
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('https://ohtaeyang95.github.io/KaKao/service-worker.js')
+        .then(function (success) {
+            console.log('[Service Worker 등록 완료]', success)
+        })
+        .catch(function (error) {
+            console.log('[Service Worker 등록 실패]', error);
+        });
+}
