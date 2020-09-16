44bd9f3054687ffe8b2458807108a56ea144542a
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-16 23:14:12 +0900

    dsfdsf

--- sw.js
+++ sw.js
@@ -44,12 +44,14 @@ export const pushSubscription = (subscription) => {
 
 
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://ohtaeyang95.github.io/KaKao/service-worker.js')
-        .then(function (success) {
-            console.log('[Service Worker 등록 완료]', success)
-        })
-        .catch(function (error) {
-            console.log('[Service Worker 등록 실패]', error);
-        });
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('https://ohtaeyang95.github.io/KaKao/service-worker.js')
+            .then(function (success) {
+                console.log('[Service Worker 등록 완료]', success)
+            })
+            .catch(function (error) {
+                console.log('[Service Worker 등록 실패]', error);
+            });
+    });
 }
