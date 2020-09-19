6c976f194f6a2432f18095f3b47459625e89360b
devos3195 <62845593+devos3195@users.noreply.github.com>
2020-09-19 13:19:36 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,7 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('https://ohtaeyang95.github.io/KaKao/service-worker.js')
         .then(function (success) {
             console.log('[Service Worker 등록 완료]', success);
+            console.log('1');
         })
         .catch(function (error) {
             console.log('[Service Worker 등록 실패]', error);
